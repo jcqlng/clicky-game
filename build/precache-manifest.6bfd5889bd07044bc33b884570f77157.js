4dbce4977d82161c61e5b211638e656da437077e
jcqlng <jcqlngeiger@outlook.com>
2020-08-18 21:14:31 -0400

    GH branch deploy

---
+++ build/precache-manifest.6bfd5889bd07044bc33b884570f77157.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dade634bb793bc77cb113e0b7b3a7a5c",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "e5c97fa8cb8bbd2db753",
+    "url": "/clicky-game/static/css/main.357a71d0.chunk.css"
+  },
+  {
+    "revision": "dc55c7eb031f5aca12d0",
+    "url": "/clicky-game/static/js/2.1dd1b36f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/clicky-game/static/js/2.1dd1b36f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5c97fa8cb8bbd2db753",
+    "url": "/clicky-game/static/js/main.d55215cf.chunk.js"
+  },
+  {
+    "revision": "a2dd66fdceb11b57b463",
+    "url": "/clicky-game/static/js/runtime-main.3c5f3143.js"
+  }
+]);
